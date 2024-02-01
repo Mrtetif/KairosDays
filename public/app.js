42968a5e25a8391a2f585b6703d1fade7f2b6726
Roberto <robertomerani@gmail.com>
2024-02-01 22:34:33 +0100

    Added the smooth animation on the ON TOP button

--- public/app.js
+++ public/app.js
@@ -29,6 +29,12 @@ window.addEventListener('scroll', function () {
 });
 
 btnTop.addEventListener('click', function () {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  function scrollToTop() {
+    var position =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    if (position) {
+      window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
+      scrollAnimation = setTimeout('scrollToTop()', 30);
+    } else clearTimeout(scrollAnimation);
+  }
 });
