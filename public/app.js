258f240fa8e1513701cf39e0fba575c8692a5aeb
Roberto <robertomerani@gmail.com>
2024-02-01 19:11:52 +0100

    Orange color Modified and TO TOP button

--- public/app.js
+++ public/app.js
@@ -1,3 +1,5 @@
+// nav bar button
+
 const navBtn = document.querySelector('.nav-btn');
 const links = document.querySelector('.nav-links');
 
@@ -12,3 +14,21 @@ navBtn.addEventListener('click', function () {
 const date = document.getElementById('date');
 const currentYear = new Date().getFullYear();
 date.textContent = currentYear;
+
+// to top button
+
+const btnTop = document.querySelector('.btn-top');
+const docScroll = document.body.scrollTop;
+
+window.addEventListener('scroll', function () {
+  if (docScroll > 80 || this.document.documentElement.scrollTop > 80) {
+    btnTop.classList.remove('none');
+  } else {
+    btnTop.classList.add('none');
+  }
+});
+
+btnTop.addEventListener('click', function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
